3fbac7e97977c86b20fbb323f8b04da3522fd91f
willin <willin@willin.org>
2017-03-02 15:04:52 +0800

    fix cdn

--- server/route/user.js
+++ server/route/user.js
@@ -1,9 +1,13 @@
 const _ = require('koa-route');
 const view = require('../view');
+const {users} = require('../../config');
 
 module.exports = (app) => {
   app.use(_.get('/:user', async (ctx, user) => {
     ctx.type = 'html';
-    ctx.body = view('user', {user});
+    ctx.body = view('user', {
+      user,
+      users: JSON.stringify(users.map(u => u.user))
+    });
   }));
 };
